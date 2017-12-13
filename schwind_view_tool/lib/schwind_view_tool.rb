a5ecfd95813c8385a1333c7edf47d95643511818
Jordan Hudgens <jordan@devcamp.com>
2017-12-13 22:55:32 +0000

    Schwind view tool first commit

---
+++ schwind_view_tool/lib/schwind_view_tool.rb
@@ -0,0 +1,6 @@
+require "schwind_view_tool/version"
+require "schwind_view_tool/renderer"
+
+module SchwindViewTool
+  # Your code goes here...
+end
---
+++ schwind_view_tool/lib/schwind_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SchwindViewTool
+  VERSION = "0.1.0"
+end
